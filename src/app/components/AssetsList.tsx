9a7940b20c621a39af55519aa1973cc49cf318e7
rayniel95 <rainyel.ramos@gmail.com>
2023-12-29 10:02:13 -0500

    fix types according to erc720

--- src/app/components/AssetsList.tsx
+++ src/app/components/AssetsList.tsx
@@ -1,12 +1,11 @@
 'use client'
 
 import { useCallback, useState } from "react";
-import { TokenBalance, TokenBalanceType, TokenMetadataResponse } from "alchemy-sdk";
+import { OwnedNft, TokenBalance, TokenBalanceType, TokenMetadataResponse } from "alchemy-sdk";
 import InfiniteScroll from "react-infinite-scroller";
 import { Spinner } from "react-bootstrap";
 import { getAlchemyClient, NetworkUrls } from "@/util/clientConfig";
 import ERC721Token from "./ERC721Token";
-import { getTokenMetadataBatch } from "@/util/batchTokenMetadata";
 
 export interface IAssetsListProps {
   chain: any;
@@ -16,7 +15,7 @@ export interface IAssetsListProps {
 type TokenResponse = TokenMetadataResponse & TokenBalance
 
 export default function AssetsList({ chain, address }: IAssetsListProps) {
-  const [items, setItems] = useState<TokenResponse[]>([]);
+  const [items, setItems] = useState<OwnedNft[]>([]);
   const [pageKey, setPageKey] = useState<string | undefined>(undefined)
    
   const hasMore = (pageKey == undefined && items.length == 0) || (pageKey != undefined && items.length > 0)
@@ -25,21 +24,17 @@ export default function AssetsList({ chain, address }: IAssetsListProps) {
 
   const fetchData = useCallback(async function()  {
     try {
-      const data = await alchemy.core.getTokenBalances(
-        address as string,
-        {
-          type: TokenBalanceType.ERC20,
-          pageKey,
-        }
-      );
-      const tokenMetadata = await getTokenMetadataBatch(
-        data.tokenBalances.map((item) => item.contractAddress),
-        NetworkUrls[alchemy.config.network],
-        alchemy.config.apiKey
-      )
-      console.log(items)
-      const newItems = items.concat(data.tokenBalances.map((item, index) => ({ ...item, ...tokenMetadata[index].result })))
-      setItems(newItems)
+      const data = await alchemy.nft.getNftsForOwner(address, {
+        omitMetadata: false,
+        pageKey
+      });
+      // const tokenMetadata = await getTokenMetadataBatch(
+      //   data.tokenBalances.map((item) => item.contractAddress),
+      //   NetworkUrls[alchemy.config.network],
+      //   alchemy.config.apiKey
+      // )
+      // console.log(items)
+      setItems(data.ownedNfts)
       setPageKey(data.pageKey)
     } catch (error) {
       console.error('Error:', error);
@@ -61,7 +56,7 @@ export default function AssetsList({ chain, address }: IAssetsListProps) {
           {items.length > 0 && items.map(
             (item, index) => (
               <li key={index}>
-                <ERC721Token symbol={item.symbol!} balance={item.tokenBalance!} logo={item.logo!} decimals={item.decimals!} />
+                <ERC721Token symbol={item.name!} balance={item.balance!} logo={item.image.pngUrl!} decimals={item.decimals!} />
                 <hr />
               </li>
             )
